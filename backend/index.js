c672d67563ea5a03d79a16ca7a46ea2aea350579
Yagna <yagna.b@git.assessmentcentre.uk>
2020-01-24 14:41:50 +0000

    uploading single path working

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log('GET at /');
-  res.send('Hello World');
+  var firstImagePath = path.resolve(__dirname, 'images/download-1.jpg');
+  res.sendFile(firstImagePath);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000..."));
