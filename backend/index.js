946eb2ab746def629e0b0e9c8da90cddd88bbf08
Yagna <yagna.b@git.assessmentcentre.uk>
2020-01-24 14:54:06 +0000

    send json array with image names

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 
 app.get('/', (req, res) => {
-  var firstImagePath = path.resolve(__dirname, 'images/download-1.jpg');
-  res.sendFile(firstImagePath);
+  var imagesDirPath = path.join(__dirname, 'images');
+  fs.readdir(imagesDirPath, (err, files) => {
+    if(!err) {
+      res.send(files);
+    }
+    else {
+      res.status(500).send();
+    }
+  });
 });
 
 app.listen(3000, () => console.log("Listening on port 3000..."));
