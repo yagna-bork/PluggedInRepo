6be0214078c58692809e0fe8591df7db1e749f97
Yagna <yagna.b@git.assessmentcentre.uk>
2020-02-04 22:11:49 +0000

    getting location before upload

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -142,22 +142,24 @@ class App extends Component<Props, State> {
   uploadImage() {
     console.log(this.state.uploadImage);
 
-
-
-    RNFetchBlob.fetch('POST', apiRootUrl + 'images', {
-      Authorization: "Bearer access-token",
-      otherHeader: "foo",
-      'Content-Type': 'multipart/form-data',
-    }, [
-      { name: 'image', filename: 'image.png', type: 'image/png', data: this.state.uploadImage.data },
-      { name: 'metadata', data : JSON.stringify({
-        lat: this.state.location.lat,
-        long: this.state.location.long
-      }) }
-    ]).then((resp) => {
-      console.log('resp: ' + resp);
-    }).catch((err) => {
-      console.log('err: ' + err);
+    this.getCurrentLocation().then(() => {
+      RNFetchBlob.fetch('POST', apiRootUrl + 'images', {
+        Authorization: "Bearer access-token",
+        otherHeader: "foo",
+        'Content-Type': 'multipart/form-data',
+      }, [
+        { name: 'image', filename: 'image.png', type: 'image/png', data: this.state.uploadImage.data },
+        {
+          name: 'metadata', data: JSON.stringify({
+            lat: this.state.location.lat,
+            long: this.state.location.long
+          })
+        }
+      ]).then((resp) => {
+        console.log('resp: ' + resp);
+      }).catch((err) => {
+        console.log('err: ' + err);
+      });
     });
   }
 
