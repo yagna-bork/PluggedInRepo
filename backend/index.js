ea57175a7970a47ca4bff05a1cb0b5881655ecca
Yagna <yagna.b@git.assessmentcentre.uk>
2020-02-05 22:56:47 +0000

    got coord in upload working

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,11 @@ const fs = require('fs');
 const exif = require('exif-parser');
 const multer = require('multer');
 const piexif = require("piexifjs");
-const FileReader = require('filereader');
+const dmsConversion = require('dms-conversion');
+
+console.log(dmsConversion.default);
+const parseDms = dmsConversion.parseDms;
+
 
 const userLatitude = 52.292016;
 const userLongitude = -1.532429;
@@ -62,41 +66,69 @@ app.post('/images', upload.single('image'), (req, res) => {
     var lat = JSON.parse(req.body.metadata).lat;
     var long = JSON.parse(req.body.metadata).long;
 
+    console.log("lat upload before conversion: " + lat);
+    console.log("long upload before conversion: " + long);
+
+    // console.log("lat converison: " + deg_to_dms(lat));
+    // console.log("long converison: " + deg_to_dms(long));
+
+    const { longitude, latitude } = ConvertDDToDMS(lat, long);
+
     var jpeg = fs.readFileSync(imgPath);
     var data = jpeg.toString("binary");
     var exifObj = piexif.load(data);
     exifObj["GPS"][piexif.GPSIFD.GPSVersionID] = [7, 7, 7, 7];
     exifObj["GPS"][piexif.GPSIFD.GPSDateStamp] = "1999:99:99 99:99:99";
     exifObj["GPS"][piexif.GPSIFD.GPSLatitudeRef] = lat < 0 ? 'S' : 'N';
-    exifObj["GPS"][piexif.GPSIFD.GPSLatitude] = degToDmsRational(lat);
+    exifObj["GPS"][piexif.GPSIFD.GPSLatitude] = latitude;
     exifObj["GPS"][piexif.GPSIFD.GPSLongitudeRef] = long < 0 ? 'W' : 'E';
-    exifObj["GPS"][piexif.GPSIFD.GPSLongitude] = degToDmsRational(long);
+    exifObj["GPS"][piexif.GPSIFD.GPSLongitude] = longitude;
 
 
-    console.log("long upload: " + exifObj["GPS"][piexif.GPSIFD.GPSLongitude]);
-    console.log("lat upload: " + exifObj["GPS"][piexif.GPSIFD.GPSLatitude]);
+    // console.log("long upload: ");
+    // console.log(exifObj["GPS"][piexif.GPSIFD.GPSLongitude]);
+    // console.log("lat upload: ");
+    // console.log(exifObj["GPS"][piexif.GPSIFD.GPSLatitude]);
 
     var exifbytes = piexif.dump(exifObj);
     var newData = piexif.insert(exifbytes, data);
     var newJpeg = new Buffer(newData, "binary");
     fs.writeFileSync(imgPath + '-location', newJpeg);
+
+    console.log("new method allowed");
   }
   res.send('Response from server');
 });
 
 //taken from https://github.com/hMatoba/piexifjs/issues/1
-function degToDmsRational(degFloat) {
-  var minFloat = degFloat % 1 * 60
-  var secFloat = minFloat % 1 * 60
-  var deg = Math.floor(degFloat)
-  var min = Math.floor(minFloat)
-  var sec = Math.round(secFloat * 100)
+// function degToDmsRational(degFloat) {
+//   var minFloat = degFloat % 1 * 60
+//   var secFloat = minFloat % 1 * 60
+//   var deg = Math.floor(degFloat)
+//   var min = Math.floor(minFloat)
+//   var sec = Math.round(secFloat * 100)
 
-  deg = Math.abs(deg) * 1
-  min = Math.abs(min) * 1
-  sec = Math.abs(sec) * 1
+//   deg = Math.abs(deg) * 1
+//   min = Math.abs(min) * 1
+//   sec = Math.abs(sec) * 1
 
-  return [[deg, 1], [min, 1], [sec, 100]]
+//   return [[deg, 1], [min, 1], [sec, 100]]
+// }
+
+function ConvertDDToDMS(lat, long) {
+  const dmsCoords = new dmsConversion.default(lat, long);
+  return dmsCoords.dmsArrays;
+}
+
+
+//taken from https://stackoverflow.com/questions/1140189/converting-latitude-and-longitude-to-decimal-values
+function ConvertDMSToDD(degrees, minutes, seconds, direction) {
+  var dd = degrees + minutes / 60 + seconds / (60 * 60);
+
+  if (direction == "S" || direction == "W") {
+    dd = dd * -1;
+  } // Don't do anything for N or E
+  return dd;
 }
 
 var readFileAndCheckDistancePromise = function(imgName, imgPath, validImageNames) {
