b7ef73f6c3624010d2ca305d288166b991fc2379
Yagna <yagna.b@git.assessmentcentre.uk>
2020-02-07 15:32:04 +0000

    got dynamically generated images working

--- frontend/components/VerticalScrollView.tsx
+++ frontend/components/VerticalScrollView.tsx
@@ -24,26 +24,12 @@ class VerticalScrollView extends Component<Props, State> {
 
     return (
       <ScrollView decelerationRate={0} snapToInterval={screenHeight} snapToAlignment={"center"}>
-        {
-          this.state.resultArray.map((item, key) => {
-          let src = this.state.foodList.find(food => +food.id === item).src;
-          return <Image key={key} style={{ flexDirection: "row" }} source={uri: src)} />;
-          })}
-
         {this.props.imageUrls.map(url => {
           return <Image 
             style={{ width: screenHeight * (1920 / 1080), height: screenHeight }}
             source={{uri: url}}
           />
         })}
-        <Image
-          style={{ width: screenHeight * (1920 / 1080), height: screenHeight }}
-          source={{ uri: this.props.imageUrls[0] }}
-        />
-        <Image
-          style={{ width: screenHeight * (1920 / 1080), height: screenHeight }}
-          source={{ uri: this.props.imageUrls[0] }}
-        />
       </ScrollView>
     );
   }
