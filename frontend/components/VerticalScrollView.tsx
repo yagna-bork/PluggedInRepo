089504e17d6512abe89ce9c1acb0878a11fa88f9
Yagna <yagna.b@git.assessmentcentre.uk>
2020-02-07 14:52:47 +0000

    boilder plate code

--- frontend/components/VerticalScrollView.tsx
+++ frontend/components/VerticalScrollView.tsx
@@ -4,3 +4,27 @@ import {
   ScrollView,
   Image, Text, View, TextInput,
   Dimensions } from 'react-native';
+
+
+interface Props {
+  imageUrls: string[]
+}
+
+interface State {
+
+}
+
+export default class VerticalScrollView extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    <ScrollView>
+      <Image
+        style={{ width: 100, height: 100 }}
+        source={{ uri: this.props.imageUrls[0] }}
+      />
+    </ScrollView>
+  }
+}
