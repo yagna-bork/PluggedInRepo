8827f093ccc4025d711b83aa253ae1f593f511a8
Yagna <yagna.b@git.assessmentcentre.uk>
2020-03-30 21:04:07 +0100

    changed app.tsx to get rid of eslint errors

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-unused-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,49 +7,25 @@
  * @flow
  */
 
-import React from 'react'
-import { Component } from 'react';
+import React, {Component} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './components/HomeScreen';
 import UploadScreen from './components/UploadScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Image,
-  TouchableOpacity,
-  PermissionsAndroid, Platform
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import { resolvePlugin } from '@babel/core';
 
 const Tab = createBottomTabNavigator();
 
 interface Props {}
-interface State { }
+interface State {}
 
 //refreshing homepage when you click on it
-function HomeScreenTab({ navigation }) {
-  return (
-    <HomeScreen/>
-  );
+function HomeScreenTab() {
+  return <HomeScreen />;
 }
 
 class App extends Component<Props, State> {
   render() {
-    return(
+    return (
       <NavigationContainer>
         <Tab.Navigator>
           <Tab.Screen name="Home" component={HomeScreenTab} />
@@ -58,44 +35,5 @@ class App extends Component<Props, State> {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
