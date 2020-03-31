f1932dc30122e15cd301a2f788c989a9d8da3a75
Yagna <yagna.b@git.assessmentcentre.uk>
2020-03-31 23:20:13 +0100

    removed eslint and ts errors verticalscrollview

--- frontend/components/VerticalScrollView.tsx
+++ frontend/components/VerticalScrollView.tsx
@@ -1,29 +1,16 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  ScrollView,
-  Image, Text, View, TextInput,
-  Dimensions,
-  TouchableOpacity,
-  ActivityIndicator
-} from 'react-native';
+import React, {Component} from 'react';
+import {ScrollView, Image, Dimensions, ActivityIndicator} from 'react-native';
+
+import {Image as LazyImage} from 'react-native-elements';
 
 interface Props {
-  images: any[],
-  apiRootUrl: string
+  images: any[];
+  apiRootUrl: string;
 }
 
-import { Image as LazyImage } from 'react-native-elements';
-
-interface State {
-
-}
+interface State {}
 
 class VerticalScrollView extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-  }
-
   render() {
     var screenHeight = Dimensions.get('window').height;
     var screenWidth = Dimensions.get('window').width;
@@ -35,46 +22,48 @@ class VerticalScrollView extends Component<Props, State> {
     }
 
     return (
-      <ScrollView decelerationRate={0} snapToInterval={screenHeight} snapToAlignment={"center"} showsVerticalScrollIndicator={false}>
+      <ScrollView
+        decelerationRate={0}
+        snapToInterval={screenHeight}
+        snapToAlignment="center"
+        showsVerticalScrollIndicator={false}>
         {/* 50 replies test horizontal scroll */}
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
           <Image
-            style={{ width: screenWidth, height: screenHeight }}
-            source={{ uri: this.props.images[0].url }}
+            style={{width: screenWidth, height: screenHeight}}
+            source={{uri: this.props.images[0].url}}
           />
           {repeatedReply.map(reply => {
-            return (<LazyImage
-              style={{ width: screenWidth, height: screenHeight }}
-              source={{ uri: reply.url }}
-              PlaceholderContent={<ActivityIndicator />}
-            />);
+            return (
+              <LazyImage
+                style={{width: screenWidth, height: screenHeight}}
+                source={{uri: reply.url}}
+                PlaceholderContent={<ActivityIndicator />}
+              />
+            );
           })}
         </ScrollView>
         {/* TODO: only load when in view port (or near?) */}
         {/* Dynamically generated parent Images and replies */}
-        {
-          this.props.images.map(img => {
-            return (
-              <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                {
-                  <Image
-                    style={{ width: screenWidth, height: screenHeight }}
-                    source={{ uri: img.url }}
+        {this.props.images.map(img => {
+          return (
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <Image
+                style={{width: screenWidth, height: screenHeight}}
+                source={{uri: img.url}}
+              />
+              {img.replies.map((reply: {url: string}) => {
+                return (
+                  <LazyImage
+                    style={{width: screenWidth, height: screenHeight}}
+                    source={{uri: reply.url}}
+                    PlaceholderContent={<ActivityIndicator />}
                   />
-                }
-                {
-                  img.replies.map(reply => {
-                    return (<LazyImage
-                      style={{ width: screenWidth, height: screenHeight }}
-                      source={{ uri: reply.url }}
-                      PlaceholderContent={<ActivityIndicator />}
-                    />);
-                  })
-                }
-              </ScrollView>
-            );
-          })
-        }
+                );
+              })}
+            </ScrollView>
+          );
+        })}
       </ScrollView>
     );
   }
